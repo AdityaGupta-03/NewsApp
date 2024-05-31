b6a3b6d2995029c90c85322e7460376b06ea982f
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-06-01 00:38:28 +0530

    Adding News and NewsItem component

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,13 +2,19 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
-  render() {
-    return (
-        <div>
-            <NewsItem />
-            <NewsItem />
-            <NewsItem />
-        </div>
-    )
-  }
+    render() {
+        return (
+            <div className='row'>
+                <div className="col-4">
+                    <NewsItem title={"Cricket"} desc={"RCB again losses 10th time in a row"} />
+                </div>
+                <div className="col-4">
+                    <NewsItem title={"Football"} desc={"RCB again losses 10th time in a row"} />
+                </div>
+                <div className="col-4">
+                    <NewsItem title={"Badminton"} desc={"RCB again losses 10th time in a row"} />
+                </div>
+            </div>
+        )
+    }
 }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
-  render() {
-    return (
-        <div>
-            
-        </div>
-    )
-  }
+
+    render() {
+        return (
+            <div className="card" style={{width:"18rem"}}>
+                <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{this.props.title}</h5>
+                        <p className="card-text">{this.props.des}</p>
+                        <a href="/Newsdetail" className="btn btn-primary">Explore</a>
+                    </div>
+            </div>
+        )
+    }
 }
