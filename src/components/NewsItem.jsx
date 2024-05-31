7597b26951876cf486d168078d9e9712f4142d27
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-06-01 01:40:06 +0530

    setting url to the news

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -11,7 +11,7 @@ export default class NewsItem extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{desc}</p>
-                        <a href={`/newsdetail/${url}`} className="btn btn-sm btn-primary">Read More</a>
+                        <a target="_blank" href={`${url}`} className="btn btn-sm btn-primary" rel='noreferrer'>Read More</a>
                     </div>
             </div>
         )
